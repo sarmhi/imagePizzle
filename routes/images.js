d3fa5cc36c56d9ffbe2bf69f4d5a9b86beac845c
Samuel <sammiejay813@gmail.com>
2022-02-24 03:05:12 +0100

    Migrated to bootstrap 5 and fixed the error handling

--- routes/images.js
+++ routes/images.js
@@ -4,7 +4,7 @@ const imageControllers = require('../controllers/images');
 
 const router = express.Router();
 
-router.get('/get-image', imageControllers.getImage);
+router.get('', imageControllers.getImage);
 
 router.post('/post-image', imageControllers.postImage);
 
